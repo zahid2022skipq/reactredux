8c4716ef967bd7f4a3d8c33687c5eda2af5bc513
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-13 15:50:24 +0500

    added navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,8 +5,8 @@ const Navbar = () => {
     <div>
       <nav className="navbar navbar-expand-lg bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar
+          <a className="navbar-brand" href="/">
+            Bank of Zahid
           </a>
           <button
             className="navbar-toggler"
@@ -22,12 +22,12 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
+                <a className="nav-link active" aria-current="page" href="/">
                   Home
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <a className="nav-link" href="/">
                   Link
                 </a>
               </li>
