4aa20ac66ca538ff70cf9549708f4f9650ce330c
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-13 17:04:16 +0500

    added indexjs to acces action creator

---
+++ src/state/index.js
@@ -0,0 +1,1 @@
+export * as actipnCreator from "./action-creators/index";
---
+++ src/state/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import amountReducer from "./amountReducer";
+
+export default reducers = combineReducers({
+  amount: amountReducer,
+});
